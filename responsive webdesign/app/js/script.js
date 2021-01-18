b51da37caf61da8708095fbe98f4f8f1dc606fe5
Leo Enslin <leo.enslin@gmx.de>
2021-01-18 12:30:11 +0100

    init

---
+++ responsive webdesign/app/js/script.js
@@ -0,0 +1,17 @@
+const btnHamburger = document.querySelector('#btnHamburger');
+const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay');
+
+btnHamburger.addEventListener('click', function() {
+    console.log('click hamburger');
+
+    if(header.classList.contains('open')){ // CLOSE HAMBURGER MENU
+        header.classList.remove('open');
+        overlay.classList.add('fade-out');
+        overlay.classList.remove('fade-in');
+    } else { // CLOSE HAMBURGER MENU
+        header.classList.add('open');
+        overlay.classList.add('fade-in');
+        overlay.classList.remove('fade-out');
+    };
+});
